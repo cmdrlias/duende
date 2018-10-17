a78b581e0762188f4ead1fad41997c391bc8d076
Larissa Silva <larissa.silva@nostrum.com.br>
2018-10-17 16:13:52 -0300

    fix

--- src/scape/Main.java
+++ src/scape/Main.java
@@ -1,8 +1,76 @@
 package scape;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+	
+	public static void localizarDuende(int[][] map, int x, int y) {
+		int dx = 0, dy = 0;
+		for(int i = 0; i < x; i++) {
+			for(int j = 0; j < y; j++) {
+				if(map[i][j] == 3) {
+					dx = i;
+					dy = j;
+				}
+			}
+		}
+		
+		System.out.print("\nAs coordenadas do duende são: " + dx + " e " + dy);
+		
+		guiarDuende(map, x, y);
+	}
+	
+	public static void guiarDuende(int map[][], int x, int y) {
+		boolean fim = false;
+		int fimX = 0, fimY = 0;
+		int dX = 0, dY = 0;
+		while(!fim) {
+			for(int i = 0; i < x; i++) {
+				for(int j = 0; j < y; j++) {
+					if(map[i][j] == 0) {
+						fimX = i;
+						fimY = j;
+					}
+					if(map[i][j] == 3) {
+						dX = i;
+						dY = j;
+						if(i > 0 && map[i-1][j] == 1 && map[i-1][j] != map[dX][dY]) {
+							map[i-1][j] = 3;
+							map[i][j] = 1;
+						} else if(i < (x-1) && map[i+1][j] == 1 && map[i+1][j] != map[dX][dY]) {
+							map[i+1][j] = 3;
+							map[i][j] = 1;
+						} else if(j > 0 && map[i][j-1] == 1 && map[i][j-1] != map[dX][dY]) {
+							map[i][j-1] = 3;
+							map[i][j] = 1;
+						}  else if(j < (y-1) && map[i][j+1] == 1 && map[i][j+1] != map[dX][dY]) {
+							map[i][j+1] = 3;
+							map[i][j] = 1;
+						}
+						if(i > 0 && map[i-1][j] == 0) {
+							fim = true;
+						} else if(i < (x-1) && map[i+1][j] == 0) {
+							fim = true;
+						} else if(j > 0 && map[i][j-1] == 0) {
+							fim = true;
+						}  else if(j < (y-1) && map[i][j+1] == 0) {
+							fim = true;
+						}
+					}
+				}
+			}
+			System.out.print("\n");
+			for(int i = 0; i < x; i++) {
+				for(int j = 0; j < y; j++) {
+					System.out.print(map[i][j] + "\t");
+				}
+				System.out.print("\n");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("saiuu");
+	}
+	
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		
@@ -19,9 +87,12 @@ public class Main {
 		System.out.print("\nPreencha o caminho do doende:\n\n");
 		for(int i = 0; i < x; i++) {
 			for(int j = 0; j < y; j++) {
+				System.out.print("Linha " + i + ", Coluna " + j + " : ");
 				map[i][j] = read.nextInt();
 			}
 		}
+		
+		System.out.print("\n");
 		
 		for(int i = 0; i < x; i++) {
 			for(int j = 0; j < y; j++) {
@@ -29,5 +100,8 @@ public class Main {
 			}
 			System.out.print("\n");
 		}
+		
+		localizarDuende(map, x, y);
+			
 	}
 }
